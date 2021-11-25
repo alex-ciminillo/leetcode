75598eb33e95e2eaacd4d36686287e89bedf609d
ciminilloa <ciminilloa@findlay.edu>
2021-11-25 10:43:37 -0500

    Finish #str_str

--- implement_strStr.rb
+++ implement_strStr.rb
@@ -26,24 +26,20 @@
 # 0 <= haystack.length, needle.length <= 5 * 104
 # haystack and needle consist of only lower-case English characters.
 
-
+# first attempt, O(n)
 def str_str(haystack, needle)
     return 0 if needle.empty?
+    length = needle.length
     hash = Hash.new { |h, k| h[k] = [] }
-    haystack.each_char.with_index { |char, idx| hash[char] << idx }
-
-    prev = needle[0]
-    needle[1..-1].each_char do |char|
-        return -1 if hash[char].empty?
-        if !hash[prev].include?(hash[char][0] + 1)
-            return -1
-        end
+    haystack.each_char.with_index do |char, idx| 
+        hash[haystack[idx...(idx + length)]] << idx 
     end
-    true
+    return -1 if hash[needle].empty?
+    hash[needle][0]
 end
 
-haystack = "help"
-needle = "lp"
+haystack = "helplelejfiorkgjfdsleekelelekelehelpelehjlkeple"
+needle = "le"
 
 p str_str(haystack, needle)
 
