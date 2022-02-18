5c86da8bbd7f83fbe9c3e5d9ff5959f018e25a36
alex-ciminillo <ciminilloa@findlay.edu>
2022-02-18 17:54:15 -0500

    Clean up solution

--- javascript/removeDup.js
+++ javascript/removeDup.js
@@ -23,13 +23,9 @@
 var deleteDuplicates = function(head) {
     if(!head) return head
     var cur = head
-    
     while(cur!=null && cur.next != null) {
-        if(cur.next.val === cur.val) {
-            cur.next = cur.next.next
-        }else {
-            cur = cur.next
-        }
+        if(cur.next.val === cur.val) { cur.next = cur.next.next }
+        else { cur = cur.next }
     }
     return head
 }
