5f0fdb2c0f921a6679a0eadf43a4279940e315e0
alex-ciminillo <ciminilloa@findlay.edu>
2022-03-24 12:41:21 -0500

    Style question

--- javascript/fizzBuzz.js
+++ javascript/fizzBuzz.js
@@ -4,7 +4,6 @@
 // answer[i] == "Fizz" if i is divisible by 3.
 // answer[i] == "Buzz" if i is divisible by 5.
 // answer[i] == i (as a string) if none of the above conditions are true.
- 
 
 // Example 1:
 
