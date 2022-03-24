f5e9c7c9263f0380495e9ec226e1e1e33342d0df
alex-ciminillo <ciminilloa@findlay.edu>
2022-03-24 12:23:34 -0500

    Add solution

--- javascript/fizzBuzz.js
+++ javascript/fizzBuzz.js
@@ -25,7 +25,25 @@
 // 1 <= n <= 104
 
 
-
+let fizzBuzz = function(n) {
+    let final=[];
+    while(n!==0){
+    if(n%3===0 && n%5===0){
+        final.push('FizzBuzz');
+    }
+    else if(n%3===0){
+        final.push('Fizz');
+    }
+    else if(n%5===0){
+        final.push('Buzz');
+    }
+    else{
+        final.push(n.toString());
+    }
+    n--;
+    }
+    return final.reverse();
+};
 
 
 
