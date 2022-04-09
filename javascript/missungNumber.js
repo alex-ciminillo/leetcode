df398499a390fc1091e9f34f2244548771e5fe88
alex-ciminillo <ciminilloa@findlay.edu>
2022-04-09 05:35:18 -0400

    Add missungNumber algo

--- javascript/missungNumber.js
+++ javascript/missungNumber.js
@@ -5,7 +5,13 @@
 
 var missingNumber = function(nums) {
   
+    let newArray = new Array(nums.length + 1).fill(-1)
     
+    for (let num of nums) {
+        newArray[num] = num
+    }
+
+    return newArray.indexOf(-1)
     
 };
 
