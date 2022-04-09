f63f139249ed77a16d3bf2356e454db8ade50df0
alex-ciminillo <ciminilloa@findlay.edu>
2022-04-09 05:18:34 -0400

    Add twoSum

---
+++ javascript/twoSum.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+var twoSum = function(nums, target) {
+    
+    map = {}
+
+    for (i = 0; i < nums.length; i++) {
+        let otherHalf = target - nums[i];
+
+        if (otherHalf in map) {
+            return [map[otherHalf], i];
+        }
+
+        map[nums[i]] = i;
+
+    }
+
+    return null;
+    
+    
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
