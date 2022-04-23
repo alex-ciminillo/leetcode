d34098bd3c5feeb25746cf950fa3fd91155a5946
alex-ciminillo <ciminilloa@findlay.edu>
2022-04-23 15:21:53 -0500

    Add buy

---
+++ javascript/max.js
@@ -0,0 +1,10 @@
+function maxSubArray(A) {
+    var prev = 0;
+    var max = -Number.MAX_VALUE;
+  
+    for (var i = 0; i < A.length; i++) {
+      prev = Math.max(prev + A[i], A[i]);
+      max = Math.max(max, prev);
+    }
+    return max;
+  }
