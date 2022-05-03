5a020047a80fe8464d0e4e039e7cf837b15c06cd
alex-ciminillo <ciminilloa@findlay.edu>
2022-05-03 17:25:46 -0500

    Condense problem

--- javascript/twoSum.js
+++ javascript/twoSum.js
@@ -4,24 +4,15 @@
 
 
 var twoSum = function(nums, target) {
-    
     map = {}
-
     for (i = 0; i < nums.length; i++) {
         let otherHalf = target - nums[i];
-
         if (otherHalf in map) {
             return [map[otherHalf], i];
         }
-
         map[nums[i]] = i;
-
     }
-
     return null;
-    
-    
-    
 };
 
 
