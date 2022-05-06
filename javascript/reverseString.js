db1c55cbb8221b5c3b44a40dfcaa08d6d72bc56d
alex-ciminillo <ciminilloa@findlay.edu>
2022-05-05 19:41:24 -0500

    Update examples

--- javascript/reverseString.js
+++ javascript/reverseString.js
@@ -5,11 +5,10 @@
  
 
 // Example 1:
-
 // Input: s = ["h","e","l","l","o"]
 // Output: ["o","l","l","e","h"]
-// Example 2:
 
+// Example 2:
 // Input: s = ["H","a","n","n","a","h"]
 // Output: ["h","a","n","n","a","H"]
  
