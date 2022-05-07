7957b5b955946801b503668872cfea81ddf0b615
alex-ciminillo <ciminilloa@findlay.edu>
2022-05-07 15:28:39 -0500

    Change answer

--- javascript/powerTwo.js
+++ javascript/powerTwo.js
@@ -4,17 +4,16 @@
 
 
 // Example 1:
-
 // Input: n = 1
 // Output: true
 // Explanation: 20 = 1
-// Example 2:
 
+// Example 2:
 // Input: n = 16
 // Output: true
 // Explanation: 24 = 16
-// Example 3:
 
+// Example 3:
 // Input: n = 3
 // Output: false
  
@@ -30,8 +29,11 @@
 
 
 var isPowerOfTwo = function(n) {
+
     if (n <= 0) return false;
+
     return (n & (n-1)) === 0 ;
+
 };
 
 
