ca3782c1e6b8426995826b3c7161dcf995b3d2f3
alex-ciminillo <ciminilloa@findlay.edu>
2022-05-15 16:32:47 -0500

    Organize solution

--- javascript/flowers.js
+++ javascript/flowers.js
@@ -1,6 +1,9 @@
 var canPlaceFlowers = function(flowerbed, n) {
+
     let current = 0; const size = flowerbed.length;
+
 	for(var i = 0; i <= size; i++) {
+
 		if (i < size && flowerbed[i] == 0) {
 			current++;
 			if (i == 0) current++;
@@ -10,6 +13,9 @@ var canPlaceFlowers = function(flowerbed, n) {
 			if (n <= 0) return true;
 			current = 0;
 		}
+
 	}
+
 	return false;
+	
 };
