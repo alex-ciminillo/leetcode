8410171e522ab8791a724aa63c8908414c98d2e1
alex-ciminillo <ciminilloa@findlay.edu>
2022-05-22 18:16:45 -0500

    Add climbstaircase

--- javascript/climbingStaircase.js
+++ javascript/climbingStaircase.js
@@ -1,30 +1,9 @@
 
-
-
-
-
 var climbStairs = function(n) {
-    
     let combins = new Array(n + 1)
-
     combins[1] = 1, combins[2] = 2
-
     for (i = 3; i <=n; i++) {
         combins[i] = combins[i -1] + combins[i - 2]
     }
-
     return combins[n]
-    
 };
-
-
-
-
-
-
-
-
-
-
-
-
