983e73418a4eee02bb57945eecdc6cb7676676bd
alex-ciminillo <ciminilloa@findlay.edu>
2022-05-24 14:51:36 -0500

    Add maxDepth

---
+++ javascript/maxDepth.js
@@ -0,0 +1,22 @@
+
+
+
+function maxDepth(root) {
+    if (root == null) {
+        return 0;
+    } else {
+        let leftHeight = maxDepth(root.left);
+        let rightHeight = maxDepth(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
